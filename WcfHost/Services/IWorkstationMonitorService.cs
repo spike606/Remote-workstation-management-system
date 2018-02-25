efacb08b455c4315ca78e3f65988010296ecda48
spike606 <krystusw@gmail.com>
2018-02-25 19:09:11 +0100

    Windows service hosting wcf - new project with basic configuration
    + New project for wcf host
    + Wcf hosting basic configuration with sample methods

---
+++ WcfHost/Services/IWorkstationMonitorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfHost.Services
+{
+    [ServiceContract]
+    public interface IWorkstationMonitorService
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
---
+++ WcfHost/Services/WorkstationMonitorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfHost.Services
+{
+    public class WorkstationMonitorService : IWorkstationMonitorService
+    {
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            return result;
+        }
+    }
+}
